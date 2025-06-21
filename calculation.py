1dacc0e9697915e68d162a4c9689c745f73660eb
geophysdimiK <87864043+geophysdimiK@users.noreply.github.com>
2025-06-21 14:49:42 +0200

    Update calculation.py

--- calculation.py
+++ calculation.py
@@ -1,7 +1,9 @@
 import finnhub
 import pandas as pd
 from datetime import datetime
+from pandas.tseries.offsets import BDay
 import os
+
 
 API_KEY = "DEIN_FINNHUB_API_KEY"
 CSV_FILE = "deposit.csv"
@@ -11,11 +13,16 @@ def download_data():
     stock = 'REBN' #ticker for REBORN Coffee 
     price = finnhub_client.quote('REBN')['c'] #Lists the closing price
     return price
+
+def is_first_trading_day():
+    today = pd.Timestamp.today().normalize()
+    first_day = pd.Timestamp(today.year, today.month, 1)
+    first_trading_day = pd.date_range(start=first_day, end=first_day + pd.Timedelta(days=7), freq="B")[0]
+    return today == first_trading_day
 	
 def invest(savings_rate):
-    weekday = datetime.today().weekday()
-    if weekday in [5, 6]:  # Samstag/Sonntag
-        return "Kein Handel am Wochenende"
+    if not is_first_trading_day():
+        return "Heute ist nicht der erste Handelstag des Monats."
 
     price = download_data()
     t = 1 #Transaction fees (in %)
